8b28895d8c2258a0ad1c2b593ef63583062fa2b0
凌莞 <i@gao4.pw>
2021-11-26 23:07:06 +0800

    2021年 11月 26日 星期五 23:07:06 CST

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -6,23 +6,4 @@ useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
-cat << EOM >> /etc/pacman.conf
-[archlinuxcn]
-Server = https://repo.archlinuxcn.org/x86_64
-
-[zhullyb]
-SigLevel = Never
-Server = https://mirror.zhullyb.top
-Server = https://arch.zhullyb.top
-
-[Clansty]
-SigLevel = Never
-Server = https://dl.lwqwq.com/repo/$arch
-EOM
-
-pacman-key --init
-pacman -Syu --noconfirm
-pacman -S --noconfirm archlinuxcn-keyring
-pacman -S --noconfirm yay
-
 sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"
