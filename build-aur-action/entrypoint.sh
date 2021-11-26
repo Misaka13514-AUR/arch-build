ccc591468a4af1f89e0a04c7106833337032f299
凌莞 <i@gao4.pw>
2021-11-26 23:35:42 +0800

    2021年 11月 26日 星期五 23:35:42 CST

--- build-aur-action/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pkgname=$1
-
-useradd builder -m
-echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-chmod -R a+rw .
-
-sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"
