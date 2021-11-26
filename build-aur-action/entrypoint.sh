15c95027cbbdd3099d4aeb2d444408a2910bebec
凌莞 <i@gao4.pw>
2021-11-26 22:58:36 +0800

    2021年 11月 26日 星期五 22:58:36 CST

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -9,6 +9,23 @@ chmod -R a+rw .
 cat << EOM >> /etc/pacman.conf
 [archlinuxcn]
 Server = https://repo.archlinuxcn.org/x86_64
+
+[arch4edu]
+SigLevel = Never
+Server = https://mirrors.tuna.tsinghua.edu.cn/arch4edu/$arch
+
+[lightquantum]
+SigLevel = Never
+Server = https://repo.lightquantum.me
+
+[zhullyb]
+SigLevel = Never
+Server = https://mirror.zhullyb.top
+Server = https://arch.zhullyb.top
+
+[Clansty]
+SigLevel = Never
+Server = https://dl.lwqwq.com/repo/$arch
 EOM
 
 pacman-key --init
