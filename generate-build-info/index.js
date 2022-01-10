9ed4963ab33784526f77ba53b791fb768e6271c8
凌莞~(=^▽^=) <hujl_123@yeah.net>
2022-01-10 18:00:54 +0800

    分架构处理打包结果

--- generate-build-info/index.js
+++ generate-build-info/index.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core');
 const fs = require('fs')
 const path = require('path')
-const subdirs = fs.readdirSync('pkgdb')
 const pkglist = JSON.parse(core.getInput('pkglist'))
 
 // 获取一个属性的所有值，适用于 DEPENDS 那种
@@ -26,26 +25,34 @@ const aurPackages = pkglist['builds-on-x86_64'].map(pkg => pkg.repo)
 
 const res = {
     buildTime: new Date().toISOString(),
-    packages: []
+    packages: {}
 }
 
-// 遍历每个文件夹
-for (const subdir of subdirs) {
-    const content = fs.readFileSync(path.join('pkgdb', subdir, 'desc'), 'utf-8')
-    const lines = content.split('\n')
-    res.packages.push({
-        name: getPkgAttr(lines, 'NAME'),
-        desc: getPkgAttr(lines, 'DESC'),
-        version: getPkgAttr(lines, 'VERSION'),
-        csize: getPkgAttr(lines, 'CSIZE'),
-        isize: getPkgAttr(lines, 'ISIZE'),
-        url: getPkgAttr(lines, 'URL'),
-        provides: getPkgAttrMulti(lines, 'PROVIDES'),
-        depends: getPkgAttrMulti(lines, 'DEPENDS'),
-        conflicts: getPkgAttrMulti(lines, 'CONFLICTS'),
-        buildTime: new Date(Number.parseInt(getPkgAttr(lines, 'BUILDDATE')) * 1000).toISOString(),
-        isAur: aurPackages.includes(getPkgAttr(lines, 'BASE')),
-    })
+const processArch = (arch) => {
+    res.packages[arch] = []
+    const subdirs = fs.readdirSync('pkgdb_' + arch)
+    // 遍历每个文件夹
+    for (const subdir of subdirs) {
+        const content = fs.readFileSync(path.join('pkgdb_' + arch, subdir, 'desc'), 'utf-8')
+        const lines = content.split('\n')
+        res.packages[arch].push({
+            name: getPkgAttr(lines, 'NAME'),
+            desc: getPkgAttr(lines, 'DESC'),
+            version: getPkgAttr(lines, 'VERSION'),
+            csize: getPkgAttr(lines, 'CSIZE'),
+            isize: getPkgAttr(lines, 'ISIZE'),
+            url: getPkgAttr(lines, 'URL'),
+            provides: getPkgAttrMulti(lines, 'PROVIDES'),
+            depends: getPkgAttrMulti(lines, 'DEPENDS'),
+            conflicts: getPkgAttrMulti(lines, 'CONFLICTS'),
+            buildTime: new Date(Number.parseInt(getPkgAttr(lines, 'BUILDDATE')) * 1000).toISOString(),
+            isAur: aurPackages.includes(getPkgAttr(lines, 'BASE')),
+        })
+    }
+}
+
+for (arch of ['x86_64', 'i686', 'aarch64', 'loongarch64']) {
+    processArch(arch)
 }
 
 fs.writeFileSync('build-info.json', JSON.stringify(res, null, 2), 'utf-8')
