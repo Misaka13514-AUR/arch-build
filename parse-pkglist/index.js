04a9523d676a54b43321874bad48b9e098332643
凌莞~(=^▽^=) <hujl_123@yeah.net>
2022-03-14 22:42:19 +0800

    retest

--- parse-pkglist/index.js
+++ parse-pkglist/index.js
@@ -3,13 +3,13 @@ const core = require('@actions/core');
 const origin = JSON.parse(core.getInput('pkglist'))
 const out = {
     'builds-on-x86_64': [
-        ...origin.x86_64.map(repo => ({
-            repo,
-            arch: 'x86_64'
-        })),
         ...origin.i686.map(repo => ({
             repo,
             arch: 'i686'
+        })),
+        ...origin.x86_64.map(repo => ({
+            repo,
+            arch: 'x86_64'
         })),
         ...origin['aarch64-packonly'].map(repo => ({
             repo,
