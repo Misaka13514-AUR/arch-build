1164e3fcd027f3d0534fe43d3942a5ebbd03c426
Clansty <i@gao4.pw>
2022-03-19 17:06:15 +0800

    大概是可以构建 core 了

---
+++ parse-archlinux-repo/index.js
@@ -0,0 +1,47 @@
+const core = require('@actions/core');
+const fs = require('fs')
+const path = require('path')
+
+// 获取一个属性的所有值，适用于 DEPENDS 那种
+const getPkgAttrMulti = (lines, attr) => {
+    let index = lines.indexOf(`%${attr}%`)
+    if (index < 0) {
+        return []
+    }
+    let res = []
+    while (lines[index + 1] !== '') {
+        index++
+        res.push(lines[index])
+    }
+    return res
+}
+
+// 获取一个属性，可以是多行的
+const getPkgAttr = (lines, attr) => getPkgAttrMulti(lines, attr).join('\n')
+
+const res = []
+const resAny = []
+
+const subdirs = fs.readdirSync('pkgdb')
+// 遍历每个文件夹
+for (const subdir of subdirs) {
+    const content = fs.readFileSync(path.join('pkgdb', subdir, 'desc'), 'utf-8')
+    const lines = content.split('\n')
+    if (getPkgAttr(lines, 'ARCH') === 'any') {
+        resAny.push({
+            pkgname: getPkgAttr(lines, 'PKGNAME'),
+            pkgbase: getPkgAttr(lines, 'PKGBASE'),
+            pkgver: getPkgAttr(lines, 'PKGVER'),
+            pkgrel: getPkgAttr(lines, 'PKGREL'),
+            epoch: getPkgAttr(lines, 'EPOCH'),
+            arch: getPkgAttr(lines, 'ARCH'),
+            filename: getPkgAttr(lines, 'FILENAME'),
+        })
+    }
+    else {
+        res.push(getPkgAttr(lines, 'BASE'))
+    }
+}
+
+core.setOutput('pkglist', res)
+core.setOutput('anylist', resAny)
